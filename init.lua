ed756359c666b6ad70a2b054829d7e5ace2999b1
Matěj Sychra <suculent@me.com>
2017-08-08 18:57:53 +0200

    minimized firmware (removed logging) to fit in RAM

--- init.lua
+++ init.lua
@@ -2,5 +2,6 @@
 local IDLE_AT_STARTUP_MS = 10000 -- debug only, 3 sec. is OK in production
 print("Will bootstrap in 10 seconds (adjustable in init.lua)...")
 tmr.alarm(1,IDLE_AT_STARTUP_MS,0,function()
+    print(node.heap())
     dofile("thinx.lua")--Write your program name in dofile
 end)
